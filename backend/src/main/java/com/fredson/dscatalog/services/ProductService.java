4bd3ff7b18656bdf6fa90c7da4542588c19a0896
Fredson Chaves <fredsonchaves07@gmail.com>
2022-02-26 13:16:05 -0300

    refact: pageable Product

--- backend/src/main/java/com/fredson/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/fredson/dscatalog/services/ProductService.java
@@ -13,6 +13,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,7 +38,7 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+    public Page<ProductDTO> findAllPaged(Pageable pageRequest) {
         Page<Product> list = repository.findAll(pageRequest);
         return list.map(product -> new ProductDTO(product));
     }
