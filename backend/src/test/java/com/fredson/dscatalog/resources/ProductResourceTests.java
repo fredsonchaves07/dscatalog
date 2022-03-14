884f119180297d3a545c47a6a7b427c4aad47596
Fredson Ribeiro <fredson.ribeiro@objective.com.br>
2022-03-14 18:55:42 -0300

    feat: add application/json media type

--- backend/src/test/java/com/fredson/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/fredson/dscatalog/resources/ProductResourceTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -37,7 +38,7 @@ public class ProductResourceTests {
 
     @Test
     public void findAllShouldReturnPage() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/products"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/products").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
