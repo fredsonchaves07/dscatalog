eaca6c4b6cdeb6f95149a303736fd719193935f9
Fredson Ribeiro <fredson.ribeiro@objective.com.br>
2022-03-14 19:33:25 -0300

    feat: add update and find product resource

--- backend/src/test/java/com/fredson/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/fredson/dscatalog/resources/ProductResourceTests.java
@@ -1,8 +1,10 @@
 package com.fredson.dscatalog.resources;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fredson.dscatalog.dto.ProductDTO;
 import com.fredson.dscatalog.factories.ProductFactory;
 import com.fredson.dscatalog.services.ProductService;
+import com.fredson.dscatalog.services.exceptions.DatabaseException;
 import com.fredson.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,10 +28,13 @@ public class ProductResourceTests {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
     @MockBean
     private ProductService productService;
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
     private ProductDTO productDTO;
     private PageImpl<ProductDTO> page;
 
@@ -37,11 +42,17 @@ public class ProductResourceTests {
     void setUp() throws Exception {
         existingId = 1L;
         nonExistingId = 100L;
+        dependentId = 3L;
         productDTO = ProductFactory.createProductDTO();
         page = new PageImpl<>(List.of(productDTO));
         when(productService.findAllPaged(ArgumentMatchers.any())).thenReturn(page);
         when(productService.findById(existingId)).thenReturn(productDTO);
         when(productService.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+        when(productService.update(eq(existingId), any())).thenReturn(productDTO);
+        when(productService.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
+        doNothing().when(productService).delete(existingId);
+        doThrow(ResourceNotFoundException.class).when(productService).delete(nonExistingId);
+        doThrow(DatabaseException.class).when(productService).delete(dependentId);
     }
 
     @Test
@@ -60,6 +71,26 @@ public class ProductResourceTests {
 
     @Test
     public void findByIdShouldReturnNotFoundWhenIdDoesNotExists() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", existingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.description").exists());
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/products/{id}", nonExistingId)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
