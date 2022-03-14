a9435b9355d1b06a1467e471337e680d6fea586a
Fredson Ribeiro <fredson.ribeiro@objective.com.br>
2022-03-14 19:44:29 -0300

    feat: first test integration

---
+++ backend/src/test/java/com/fredson/dscatalog/services/ProductServiceIntegrationTests.java
@@ -0,0 +1,41 @@
+package com.fredson.dscatalog.services;
+
+import com.fredson.dscatalog.repositories.ProductRepository;
+import com.fredson.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIntegrationTests {
+
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteResourceWhenIdExists() {
+        productService.delete(existingId);
+        Assertions.assertEquals(countTotalProducts - 1, productRepository.count());
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            productService.delete(nonExistingId);
+        });
+    }
+}
