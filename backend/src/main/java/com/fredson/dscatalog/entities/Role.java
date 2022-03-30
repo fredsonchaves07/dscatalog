65cbf2179ab4006586c516b85b34db91ec0ff34b
Fredson Ribeiro <fredsonchaves07@gmail.com>
2022-03-30 13:50:03 -0300

    feat: orm mapping and database seed

--- backend/src/main/java/com/fredson/dscatalog/entities/Role.java
+++ backend/src/main/java/com/fredson/dscatalog/entities/Role.java
@@ -1,11 +1,17 @@
 package com.fredson.dscatalog.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_role")
 public class Role implements Serializable {
 
     public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
