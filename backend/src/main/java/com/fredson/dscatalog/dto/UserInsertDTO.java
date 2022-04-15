8f60ea376504e801254c7138a8600c44dbcc4456
Fredson Chaves <fredsonchaves07@gmail.com>
2022-04-15 11:32:59 -0300

    feat: add custom validation

--- backend/src/main/java/com/fredson/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/fredson/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.fredson.dscatalog.dto;
 
+import com.fredson.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
     private String password;
---
+++ backend/src/main/java/com/fredson/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,38 @@
+package com.fredson.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.fredson.dscatalog.dto.UserInsertDTO;
+import com.fredson.dscatalog.entities.User;
+import com.fredson.dscatalog.repositories.UserRepository;
+import com.fredson.dscatalog.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+        User user = userRepository.findByEmail(dto.getEmail());
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe"));
+        }
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
